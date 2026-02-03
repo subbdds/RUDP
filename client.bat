46a690c07bbe812f615cc9d70158ec8acc59ae34
TBUser <subbdds@gmail.com>
2026-02-03 19:41:35 +0300

    .

--- client.bat
+++ client.bat
@@ -1,13 +1,10 @@
 @echo off
 if "%~1"=="" goto error
 
-:: Create bin if not exists
 if not exist bin mkdir bin
 
-:: Compile (Quietly)
 javac -d bin -sourcepath src\main\java src\main\java\org\subbdds\compile\*.java
 
-:: Run: %1=File, %2=IP, %3=OptionalDestName
 java -cp bin org.subbdds.compile.UDPClient %1 %2 %3
 goto end
 
